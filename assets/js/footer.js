12d41a7f8d554e2d108115c42d99db50516706ee
Ethan Rietz <ewrietz@gmail.com>
2021-07-08 17:48:41 -0500

    add crypto donations

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,6 +1,6 @@
 document.write('\
     <footer class="footer">\
-      <a href="https://github.com/erietz"><i class="fa fa-github fa-lg" aria-hidden="true" target="_blank"></i></a>\
+      <a href="https://github.com/erietz"><i class="fab fa-github fa-lg" aria-hidden="true" target="_blank"></i></a>\
       <a href="/assets/files/rietzCV.pdf"><i class="fa fa-user fa-lg" aria-hidden="true" target="_blank"></i></a>\
       <a href="mailto:ewrietz@gmail.com"><i class="fa fa-envelope fa-lg" aria-hidden="true" target="_blank"></i></a>\
       <div class="footer-text">\
